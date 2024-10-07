5803de452ab4cdd6501a98b233412c01af996306
sarvesh.sri00 <sarvesh.sri00@gmail.com>
2024-10-07 00:17:43 +0000

    welcome to the flipzon project. It's an Ecommerce website built with Next.js and Tailwind CSS

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <>
-    <div>
-      <h1>home page</h1>
-    </div>
-   </>
-    
+    <>
+      <div className="flex items-center justify-center bg-background bg-gray-100 p-6">
+        <div className="w-100">
+          <h1 className="font-extrabold text-4xl text-center mb-5 ">
+            Flipzon - Your Ultimate Shopping Destination
+          </h1>
+          <p className="text-gray-700 text-lg leading-relaxed max-w-5xl">
+            Discover a world of amazing products and unbeatable deals at
+            Flipzon. We offer a wide selection of items, from fashion and
+            electronics to home goods and more. Shop with confidence and enjoy a
+            seamless online shopping experience.
+          </p>
+        </div>
+      </div>
+      <div className="max-w-lg mt-1"></div>
+    </>
   );
 }
